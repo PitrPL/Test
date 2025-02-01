7dd50386d414068baa0d228b072a53ee24aab909
PitrPL <pitr2003@vp.pl>
2025-02-01 22:51:38 +0100

    Python Kalkulator Prosty (PKP)

---
+++ kalkulator.py
@@ -0,0 +1,9 @@
+a = float(input("Podaj wartosc A: "))
+b = float(input("Podaj wartosc B: "))
+
+print(f"Suma: {a + b}")
+print(f"Roznica: {a - b}")
+print(f"Iloczyn: {a * b}")
+print(f"Iloraz: {a / b}")
+print(f"Modulo: {a % b}")
+print(f"Potega: {a ** b}")
